28936e295636d814a8b649c685b3a6c03eaebe4a
polter1337 <marcuslitt@hotmail.com>
2019-11-28 15:32:26 +0100

    fixed get_recipe function

--- source/Recipe.c
+++ source/Recipe.c
@@ -2,20 +2,43 @@
 #include "string.h"
 #include "stdlib.h"
 
-Recipe *get_recipe(char *ingredient, Recipe *recipelist, int amount){
-    int i, strcompare, j = 0;
-    Recipe *recipeOutput = malloc(sizeof(Recipe)*amount);
+int randomGen(int max){
+    int random;
 
-    for(i = 0; i > amount; i++){
-        strcompare = strcmp(ingredient, recipelist[i].ingredients);
-            if(strcompare = 1){
-                *recipeOutput[j] = recipelist[i];
+    if(max > 0){
+        random = (rand() % max);
+        return random;
+    } else{
+        return 0;
+    }
+}
+
+Recipe get_recipe(Ingredient ingred, Recipe *recipelist, int amount){
+    int i, strcompare, j = 0, randnum;
+    Recipe *recipeArray = malloc(sizeof(Recipe)*amount);
+    Recipe recipeOutput;
+    Ingredient *ingred2 = malloc(sizeof(Ingredient)*amount);
+
+    for(i = 0; i < amount; i++)
+            ingred2[i] = *recipelist[i].ingredients;
+
+    for(i = 0; i < amount; i++){
+        strcompare = strcmp(ingred.name, ingred2[i].name);
+            if(strcompare == 0){
+                recipeArray[j] = recipelist[i];
                 j++;
             }
     }
 
-    return *recipeOutput;
+    randnum = randomGen(j);
+
+    recipeOutput = recipeArray[randnum];
+
+    free(ingred2);
+    free(recipeArray);
+    return recipeOutput;
 }
+
 
 Recipe *readRecipe(){
     FILE *file = openFile("files/recipes.txt", "r");
@@ -27,3 +50,4 @@ Recipe *readRecipe(){
     fclose(file);
     return NULL;
 }
+
