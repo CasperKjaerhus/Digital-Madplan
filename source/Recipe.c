a22066a0438dcf06ddc93c768b58032aa5184a43
polter1337 <marcuslitt@hotmail.com>
2019-11-28 13:30:24 +0100

    Recipe update

--- source/Recipe.c
+++ source/Recipe.c
@@ -1,3 +1,18 @@
 #include "Recipe.h"
+#include "string.h"
+#include "stdlib.h"
 
+Recipe *get_recipe(char *ingredient, Recipe *recipelist, int amount){
+    int i, strcompare, j = 0;
+    Recipe *recipeOutput = malloc(sizeof(Recipe)*amount);
 
+    for(i = 0; i > amount; i++){
+        strcompare = strcmp(ingredient, recipelist[i].ingredients);
+            if(strcompare = 1){
+                *recipeOutput[j] = recipelist[i];
+                j++;
+            }
+    }
+
+    return *recipeOutput;
+}
