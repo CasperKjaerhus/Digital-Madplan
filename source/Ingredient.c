6cb3a6f658d0bd0152ecbeda9926c4913d7a2e09
polter1337 <marcuslitt@hotmail.com>
2019-12-03 14:10:36 +0100

    fixed some logical errors in ingredient.c

--- source/Ingredient.c
+++ source/Ingredient.c
@@ -2,15 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+/*Function that searches for a ingredient char and finds the corresponding ingrendient struct*/
 Ingredient get_ing_struct(Ingredient *ingArray, int arrSize, char *ingredient_name){
     int i, strcompare;
     Ingredient *tempIngArr = malloc(sizeof(Ingredient)*arrSize), ingredientOutput;
 
-    for(i = 0; i > arrSize; i++){
+    for(i = 0; i < arrSize; i++){
         tempIngArr[i] = ingArray[i];
     }
 
-    for(i = 0; i > arrSize; i++){
+    for(i = 0; i < arrSize; i++){
         strcompare = strcmp(tempIngArr[i].name, ingredient_name);
             if(strcompare == 0){
                 ingredientOutput = tempIngArr[i];
@@ -19,3 +21,4 @@ Ingredient get_ing_struct(Ingredient *ingArray, int arrSize, char *ingredient_na
             }
     }
 }
+
