7920ddf7e0203bda55c7e0512d02ad7e4872c5b5
Nima Moayedzadeh <57714574+Nima-Moayedzadeh@users.noreply.github.com>
2019-12-03 14:08:40 +0100

    my name is nima i made ingredient

--- source/Ingredient.c
+++ source/Ingredient.c
@@ -1,10 +1,36 @@
 #include "Ingredient.h"
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
+#define MAXCHAR 1000
+
+int main(void){    
+char str[MAXCHAR];
+
+FILE *recipe = fopen("ingredients.txt", "r");
+
+
+if(recipe == NULL){
+  printf("Could not open file %s", recipe);
+  return 1;
+}    
+
+while (fgets(str, MAXCHAR, recipe) != NULL){
+
+
+  printf("Read from file: %s\n", str);
+}
+fclose(recipe);
+return 0;
+
+}
+
 
 
 void createIngredient(char *name, int amount){
     Ingredient newIngredient;
     
     newIngredient.name = malloc(strlen(name));
+}
+
 }
--- source/Ingredient.h
+++ source/Ingredient.h
@@ -1,5 +1,8 @@
+enum category{meat, vegetables, fruit, carbohydrates, spices};
+
 typedef struct Ingredient{
     char *name;
     int amount;
+    enum category;
     
 } Ingredient;
