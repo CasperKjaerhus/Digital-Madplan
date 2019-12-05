eb1006228366f205e12bfe2d597bf057801df1a2
polter1337 <49211975+polter1337@users.noreply.github.com>
2019-12-05 12:57:27 +0100

    Update Recipe.c

--- source/Recipe.c
+++ source/Recipe.c
@@ -4,23 +4,14 @@
 
 /*Function that returns a random number. Used for get_recipe function*/
 int randomGen(int max){
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 
     if(max > 0){
         return (rand() % max);
     } else{
     if(max > 0){
-=======
     if(max > 0){
->>>>>>> fe114ef3dd46eba28d8df8f98fde0d3f6ba3c8fa
-=======
     if(max > 0){
->>>>>>> fe114ef3dd46eba28d8df8f98fde0d3f6ba3c8fa
-=======
     if(max > 0){
->>>>>>> fe114ef3dd46eba28d8df8f98fde0d3f6ba3c8fa
         return rand() % max;
     } else {
         return 0;
