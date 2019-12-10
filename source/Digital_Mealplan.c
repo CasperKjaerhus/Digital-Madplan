3f8701d6ed098b65a59968bd0cd17f4614cb71d5
CasperKjaerhus <casperkjaerhusleth@gmail.com>
2019-12-10 20:30:24 +0100

    Removed bad code

--- source/Digital_Mealplan.c
+++ source/Digital_Mealplan.c
@@ -10,48 +10,12 @@
 int main(void){
     int recipe_amount, i;
     Recipe *recipes;
-    char answer;
-    int answer1;
-    char weekdays[7] = [Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday]
     srand(time(NULL));
 
     recipes = readRecipes(&recipe_amount);
     printRecipes(recipes, recipe_amount);
 
-    printf("Do you want to generate a mealplan? Type y for yes, Type anything else for no");
-    scanf("%c", &answer);
-
-    if(answer == 'y'){
-        printf("/*Mealplan for the Week*/");
-        for(i = 0; i < WEEK; i++){
-        printf("%s: %s", weekdays[i], recipes[i].name);
-        }
-    }
-    else
-        printf("Mealplan wasn't generated");
-    /* Giver mulighed for at ændre en enkelt ret, i den genererede mealplan*/
-    printf("If you wish to change a dish, please enter the number of the day, you would like to change");
-    scanf("%d", &answer1);
-
-    if(answer1 == 1)
-        printf("%s: %s", weekdays[answer1-1], recipes[7].name);
-    else if (answer1 == 2)
-        printf("%s: %s", weekdays[answer1-1], recipes[7].name);
-    else if (answer1 == 3)
-        printf("%s: %s", weekdays[answer1-1], recipes[7].name);
-    else if (answer1 == 4)
-        printf("%s: %s", weekdays[answer1-1], recipes[7].name);
-    else if (answer1 == 5)
-        printf("%s: %s", weekdays[answer1-1], recipes[7].name);
-    else if (answer1 == 6)
-        printf("%s: %s", weekdays[answer1-1], recipes[7].name);
-    else if (answer1 == 7)
-        printf("%s: %s", weekdays[answer1-1], recipes[7].name);
-
-
-        printf("Shopping list:");
 
     freeRecipes(&recipes, recipe_amount);
     return 0;
 }
-
