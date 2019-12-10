ff7dac3302513181c1d94be3ef130bfc933c1251
polter1337 <marcuslitt@hotmail.com>
2019-12-10 13:50:35 +0100

    Co-authored-by: Nima Moayedzadeh <nima.mzadeh@gmail.com>

--- source/ShoppingList.h
+++ source/ShoppingList.h
@@ -1,5 +1,4 @@
 #include "Recipe.h"
-#include "Ingredient.h"
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -7,3 +6,5 @@ typedef struct shoppinglist{
     char *ingredient_name;
     int amount;
 } Shoppinglist;
+
+Shoppinglist *shopping_list(Recipe *recipe_input, int list_size);
