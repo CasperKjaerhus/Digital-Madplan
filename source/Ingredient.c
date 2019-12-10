b423a4bb242dc8881febadef3e60b3da6dd9116e
polter1337 <marcuslitt@hotmail.com>
2019-12-10 11:39:41 +0100

    NEW INGREDIENT FILE

--- source/Ingredient.c
+++ source/Ingredient.c
@@ -2,19 +2,10 @@
 #include <stdio.h>
 #include <string.h>
 #include "Ingredient.h"
-/*
+#include <string.h>
 
-<<<<<<< HEAD
 /*Function that searches for a ingredient char and finds the corresponding ingrendient struct*/
-/*Ingredient get_ing_struct(Ingredient *ingArray, int arrSize, char *ingredient_name){
-=======
-<<<<<<< HEAD
 Ingredient get_ing_struct(Ingredient *ingArray, int arrSize, char *ingredient_name){
-=======
-/*Function that searches for a ingredient char and finds the corresponding ingrendient struct*/
-/*Ingredient get_ing_struct(Ingredient *ingArray, int arrSize, char *ingredient_name){
->>>>>>> 548735baa28c36e91ef634871fbf8949a818c1d5
->>>>>>> 6afbbbbbe36f82165aaf65e411b3b408274b74ea
     int i, strcompare;
     Ingredient *tempIngArr = malloc(sizeof(Ingredient)*arrSize), ingredientOutput;
 
@@ -27,24 +18,13 @@ Ingredient get_ing_struct(Ingredient *ingArray, int arrSize, char *ingredient_na
         for(i = 0; i < arrSize; i++){
             strcompare = strcmp(tempIngArr[i].name, ingredient_name);
                 if(strcompare == 0){
+                    ingredientOutput = tempIngArr[i];
                     free(tempIngArr);
                     return ingredientOutput;
                 }
         }
-<<<<<<< HEAD
     
+    free(tempIngArr);
     return ingredientOutput;
 }
-=======
 
-    return *tempIngArr;
-}
-*/
-void createIngredient(char *name, int amount){
-    //Ingredient newIngredient;
-    
-    //newIngredient.name = malloc(strlen(name));
-}
-
->>>>>>> 6afbbbbbe36f82165aaf65e411b3b408274b74ea
-*/
