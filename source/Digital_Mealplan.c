03cb0a4121ab5fce6323f5c987060a44165f90dd
magnusglinvad <57714593+magnusglinvad@users.noreply.github.com>
2019-12-11 14:26:44 +0100

    Update Digital_Mealplan.c

--- source/Digital_Mealplan.c
+++ source/Digital_Mealplan.c
@@ -8,14 +8,28 @@
 
 
 int main(void){
-    int recipe_amount, i;
+    int recipe_amount, i,n;
     Recipe *recipes;
     srand(time(NULL));
 
     recipes = readRecipes(&recipe_amount);
     printRecipes(recipes, recipe_amount);
 
+    printf("Mealplan Generator\n");
+    printf("Choose one of the following two options:\n");
+    printf("Press 1 to create a new Mealplan.\n");
+    printf("Press 2 to access your previous Mealplan.\n");
+    scanf("%d", &n);
 
+    if(n == 1){
+        
+    }
+    else if(n == 2){
+
+    }
+    else
+        printf("Invalid input");
+    
     freeRecipes(&recipes, recipe_amount);
     return 0;
 }
