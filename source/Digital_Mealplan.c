6978eac43279dba533b6991c5cc50afda42c72f9
Kasper Leander Helmer Pedersen <kasper0207@gmail.com>
2019-12-12 13:53:45 +0100

    UPs

--- source/Digital_Mealplan.c
+++ source/Digital_Mealplan.c
@@ -10,7 +10,7 @@
 
 int main(void){
     int recipe_amount, mealplan_recipe_amount = 7;
-    int i, j,n = 0, people = 1, p;
+    int i, j,l,n = 0, people = 1, p;
     char m;
     Recipe *recipes;
     Recipe *mealplan;
@@ -24,9 +24,9 @@ int main(void){
     printf("\n\n");
 
     print_shoppinglist(mealplan, 7);*/
-       
-    recipes = readRecipes(&recipe_amount, "files/recipes.txt");
     /*printRecipes(recipes, recipe_amount); */
+
+    recipes = readRecipes(&recipe_amount, "files/recipes.txt");
     
     while(n == 0||n == 1|| n == 2){
         printf("Mealplan Generator\n");
@@ -36,7 +36,7 @@ int main(void){
         scanf(" %d", &n);
 
         if(n == 1){
-            printf("How many people are the mealplan for?");
+            printf("How many people are the mealplan for?\n");
             scanf(" %d", &people);
             mealplan = GenerateMealplan(recipes, recipe_amount);
             for(j = 0; j < 7; j++){
@@ -44,12 +44,15 @@ int main(void){
                     mealplan[j].ingredients[p].amount = mealplan[j].ingredients[p].amount * people;
                 }
             }
-            printRecipes(mealplan, mealplan_recipe_amount);
+            /*printRecipes(mealplan, mealplan_recipe_amount);*/
+            for(l = 0; l < 7; l++){
+            printf("DAY %d: %s", l, mealplan[l].name);
+            }
             recipe_to_file(mealplan, mealplan_recipe_amount);
         }
         else if(n == 2){
             mealplan = readRecipes(&mealplan_recipe_amount, "files/printmealplan.txt");
-            printRecipes(mealplan, mealplan_recipe_amount);
+            /*printRecipes(mealplan, mealplan_recipe_amount);*/
         }
         else{
             printf("Invalid input");
@@ -63,7 +66,7 @@ int main(void){
                 scanf(" %d", &i);
                 if(i >= 1 && i <= 7){
                     mealplan[i-1] = getWeightedRecipe(recipes, recipe_amount, mealplan, 7);
-                    printRecipes(mealplan, mealplan_recipe_amount);
+                    printf("DAY %d: %s", i, mealplan[i].name);
                 }
             }else if(m == 'n'){
                 break;
