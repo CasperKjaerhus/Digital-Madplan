30be31c7688b3eaca419254bee5605d0b6439711
CasperKjaerhus <casperkjaerhusleth@gmail.com>
2019-12-15 19:04:36 +0100

    Fixed not being able to change amount of people multiple times!
    Also general code cleanup

--- source/TerminalInterface.c
+++ source/TerminalInterface.c
@@ -3,17 +3,16 @@
 #include "FileHandler.h"
 #include "Mealplan.h"
 #include "Recipe.h"
-#include <time.h>
 #include "ShoppingList.h"
 #include "TerminalInterface.h"
-#define WEEK 7
 
 void new_mealplan(Recipe *recipes, int amount_of_recipes){
     Recipe *mealplan;
+    int people_amount = 1;
 
     mealplan = GenerateMealplan(recipes, amount_of_recipes);
 
-    amount_of_people(mealplan);
+    amount_of_people(mealplan, &people_amount);
     showIng_changeMeal(recipes, mealplan, amount_of_recipes);
 
     free(mealplan);
@@ -21,18 +20,18 @@ void new_mealplan(Recipe *recipes, int amount_of_recipes){
 
 void previous_mealplan(int *mealplan_recipe_amount, Recipe *recipes, int amount_of_recipes){
     Recipe *mealplan;
+    int people = 1;
 
     mealplan = readRecipes(&mealplan_recipe_amount, "files/printmealplan.txt");
-    last_mealplan(recipes, mealplan, amount_of_recipes); /*Der er noget der fejler her!*/
+    last_mealplan(recipes, mealplan, amount_of_recipes, &people);
     recipe_to_file(mealplan, 7);
     
     free(mealplan);
 }
 /* Se nedenstående funktioner showIng_changeMeal og amount_of_people - denne funktion samler disse */
-void last_mealplan(Recipe *recipes, Recipe *mealplan, int amount_of_recipes){
+void last_mealplan(Recipe *recipes, Recipe *mealplan, int amount_of_recipes, int *people){
     int u;
     int mealplan_recipe_amount = 7;
-    int people;
 
     printMealplan(mealplan, 7);
 
@@ -40,19 +39,13 @@ void last_mealplan(Recipe *recipes, Recipe *mealplan, int amount_of_recipes){
     printf("1) To see ingredients on a meal OR to change a meal on the current mealplan.\n2) To see current mealplan.\n3) Change amount of people that the mealplan is for.\n");
     scanf(" %d", &u);
     
-    if(u == 1){
+    if(u == 1)
         showIng_changeMeal(recipes, mealplan, amount_of_recipes);
-    }else if(u == 2){
+    else if(u == 2)
         printMealplan(mealplan, mealplan_recipe_amount);
-    }else if(u == 3){
-        printf("How many people is the mealplan for?\n");
-        scanf(" %d", &people);
-            for(int i = 0; i < 7; i++){
-                for(int j = 0; j < mealplan[i].amount_of_ingredients; j++){
-                        mealplan[i].ingredients[j].amount *= people;
-                }
-            }
-    }else
+    else if(u == 3)
+        amount_of_people(mealplan, people);
+    else
         printf("invalid input - returning to the main menu...");
 }
 
@@ -76,7 +69,10 @@ void showIng_changeMeal(Recipe *recipes, Recipe *mealplan, int recipe_amount){
             if(dish >= 1 && dish <= 7){
                 printf("\n%s\n", mealplan[dish - 1].name);
                 for(int s = 0; s < mealplan[dish - 1].amount_of_ingredients; s++){
-                    printf("%s %lf %s\n", mealplan[dish - 1].ingredients[s].name, mealplan[dish - 1].ingredients[s].amount, mealplan[dish - 1].ingredients[s].unit);
+                    if(mealplan[dish - 1].ingredients[s].amount > 0.0)
+                        printf("%s %.1lf %s\n", mealplan[dish - 1].ingredients[s].name, mealplan[dish - 1].ingredients[s].amount, mealplan[dish - 1].ingredients[s].unit);
+                    else
+                        printf("%s %s\n", mealplan[dish - 1].ingredients[s].name, mealplan[dish - 1].ingredients[s].unit);
                 }
             }
         }else if(d == 2){
@@ -94,20 +90,34 @@ void showIng_changeMeal(Recipe *recipes, Recipe *mealplan, int recipe_amount){
 
 /* Funktion der giver brugeren mulighed for at tilpasse den genereret madplan til et bestemt antal personer */
 /* Ingredienserne bliver dermed ganget op med dette tal (de premade opskrifter er lavet for 1 person) */
-void amount_of_people(Recipe *mealplan){
+void amount_of_people(Recipe *mealplan, int *people_amount){
     int people, mealplan_recipe_amount = 7;
 
     printf("How many people is the mealplan for?\n");
     scanf(" %d", &people);
     
-    for(int i = 0; i < 7; i++){
-        for(int j = 0; j < mealplan[i].amount_of_ingredients; j++){
-             mealplan[i].ingredients[j].amount *= people;
-        }
-    }
+    updateAmountOfPeople(mealplan, *people_amount, people);
+    *people_amount = people;
     /* Vi printer den nye mealplan, som indeholder den udskiftede opskrift */
     printMealplan(mealplan, 7);
     recipe_to_file(mealplan, mealplan_recipe_amount);
+}
+
+void updateAmountOfPeople(Recipe *mealplan, int amount_currently, int amount_to_change){
+    /*First reset amount so it can be changed to correct amount of people*/
+    if(amount_currently != 1){
+        for(int i = 0; i < 7; i++){
+            for(int j = 0; j < mealplan[i].amount_of_ingredients; j++){
+                mealplan[i].ingredients[j].amount /= amount_currently;
+            }
+        }
+    }
+    /*Now update amount!*/
+    for(int i = 0; i < 7; i++){
+        for(int j = 0; j < mealplan[i].amount_of_ingredients; j++){
+            mealplan[i].ingredients[j].amount *= amount_to_change;
+         }
+    }
 }
 
 /* Formateret printning af madplanen */ 
--- source/TerminalInterface.h
+++ source/TerminalInterface.h
@@ -9,9 +9,10 @@
 #include "ShoppingList.h"
 
 void new_mealplan(Recipe *recipes, int amount_of_recipes);
-void amount_of_people(Recipe *mealplan);
+void amount_of_people(Recipe *mealplan, int *people_amount);
 void printMealplan(Recipe *mealplan, int amount);
-void last_mealplan(Recipe *recipes, Recipe *mealplan, int amount_of_recipes);
+void last_mealplan(Recipe *recipes, Recipe *mealplan, int amount_of_recipes, int *people);
 void previous_mealplan(int *mealplan_recipe_amount, Recipe *recipes, int amount_of_recipes);
 void showIng_changeMeal(Recipe *recipes, Recipe *mealplan, int recipe_amount);
+void updateAmountOfPeople(Recipe *mealplan, int amount_currently, int amount_to_change);
 #endif
